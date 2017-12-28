ea02494f68ef5bdfca47b9a5bd24e3d6e2109cbe
cdev-tux <cdev-tux@users.noreply.github.com>
2017-12-28 15:30:57 -0700

    Filter connect packet length to avoid overflows
    in dynamic huffman decoder. Improve overflow handling in
    MSG_ReadBits/MSG_WriteBits instead of checking before writing
    each bit - we allocate slightly larger buffer to properly handle
    longest possible overflow and do just one post-fact check at the
    end.
    
    Code cleanup
    
    This commit readds commit 6055159 with corrections pointed out by ec-.
    
    ec-/Quake3e@3743137

--- code/client/cl_net_chan.c
+++ code/client/cl_net_chan.c
@@ -52,21 +52,21 @@ static void CL_Netchan_Encode( msg_t *msg ) {
 		return;
 	}
 
-        srdc = msg->readcount;
-        sbit = msg->bit;
-        soob = msg->oob;
-        
-        msg->bit = 0;
-        msg->readcount = 0;
-        msg->oob = 0;
-        
-        serverId = MSG_ReadLong(msg);
+	srdc = msg->readcount;
+	sbit = msg->bit;
+	soob = msg->oob;
+
+	msg->bit = 0;
+	msg->readcount = 0;
+	msg->oob = 0;
+
+	serverId = MSG_ReadLong(msg);
 	messageAcknowledge = MSG_ReadLong(msg);
 	reliableAcknowledge = MSG_ReadLong(msg);
 
-        msg->oob = soob;
-        msg->bit = sbit;
-        msg->readcount = srdc;
+	msg->oob = soob;
+	msg->bit = sbit;
+	msg->readcount = srdc;
         
 	string = (byte *)clc.serverCommands[ reliableAcknowledge & (MAX_RELIABLE_COMMANDS-1) ];
 	index = 0;
@@ -100,19 +100,20 @@ CL_Netchan_Decode
 static void CL_Netchan_Decode( msg_t *msg ) {
 	long reliableAcknowledge, i, index;
 	byte key, *string;
-        int	srdc, sbit, soob;
+	int	srdc, sbit;
+	qboolean soob;
 
-        srdc = msg->readcount;
-        sbit = msg->bit;
-        soob = msg->oob;
-        
-        msg->oob = 0;
-        
-	reliableAcknowledge = MSG_ReadLong(msg);
+	srdc = msg->readcount;
+	sbit = msg->bit;
+	soob = msg->oob;
 
-        msg->oob = soob;
-        msg->bit = sbit;
-        msg->readcount = srdc;
+	msg->oob = qfalse;
+
+	reliableAcknowledge = MSG_ReadLong( msg );
+
+	msg->oob = soob;
+	msg->bit = sbit;
+	msg->readcount = srdc;
 
 	string = (byte *) clc.reliableCommands[ reliableAcknowledge & (MAX_RELIABLE_COMMANDS-1) ];
 	index = 0;
