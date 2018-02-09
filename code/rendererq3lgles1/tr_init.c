3dc313fb856a245281239580fcf62eae25487abb
cdev-tux <cdev-tux@users.noreply.github.com>
2018-02-09 13:51:29 -0700

    Unlatch r_fullscreen for OpenGL renderers.

--- code/rendererq3lgles1/tr_init.c
+++ code/rendererq3lgles1/tr_init.c
@@ -1067,7 +1067,7 @@ void R_Register( void )
 	r_fullscreen = ri.Cvar_Get( "r_fullscreen", "1", CVAR_ARCHIVE | CVAR_LATCH );
 #else
 	r_mode = ri.Cvar_Get( "r_mode", "3", CVAR_ARCHIVE | CVAR_LATCH );
-	r_fullscreen = ri.Cvar_Get( "r_fullscreen", "0", CVAR_ARCHIVE | CVAR_LATCH );
+	r_fullscreen = ri.Cvar_Get( "r_fullscreen", "0", CVAR_ARCHIVE );
 #endif
 	r_noborder = ri.Cvar_Get("r_noborder", "0", CVAR_ARCHIVE | CVAR_LATCH);
 	r_customwidth = ri.Cvar_Get( "r_customwidth", "1600", CVAR_ARCHIVE | CVAR_LATCH );
