ae5e195b3f4516cfe04b8d70ac2d4437d4155ab6
cdev-tux <cdev-tux@users.noreply.github.com>
2018-10-07 14:21:09 -0600

    Fix MSG_Read*String*() functions not being able to read last byte
    from message.
    
    This is exact root of q3msgboom bug:
    http://aluigi.altervista.org/adv/q3msgboom-adv.txt
    
    Unfortunately, server still needs this ugly '1022 char limit' hack to
    support unfixed clients to some degree. As it affects
    MSG_ReadBigString() - unfixed clients can still be crashed by
    8191-chars long configstrings that comes with gamestate.
    
    ioquake/ioq3@9f294ce
    ec-/Quake3e@7a971ab

--- code/qcommon/msg.c
+++ code/qcommon/msg.c
@@ -449,12 +449,14 @@ char *MSG_ReadString( msg_t *msg ) {
 		if ( c > 127 ) {
 			c = '.';
 		}
-
-		string[l] = c;
-		l++;
-	} while (l < sizeof(string)-1);
+		// break only after reading all expected data from bitstream
+		if ( l >= sizeof(string)-1 ) {
+			break;
+		}
+		string[l++] = c;
+	} while (1);
 	
-	string[l] = 0;
+	string[l] = '\0';
 	
 	return string;
 }
@@ -477,12 +479,14 @@ char *MSG_ReadBigString( msg_t *msg ) {
 		if ( c > 127 ) {
 			c = '.';
 		}
-
-		string[l] = c;
-		l++;
-	} while (l < sizeof(string)-1);
+		// break only after reading all expected data from bitstream
+		if ( l >= sizeof(string)-1 ) {
+			break;
+		}
+		string[l++] = c;
+	} while (1);
 	
-	string[l] = 0;
+	string[l] = '\0';
 	
 	return string;
 }
@@ -505,12 +509,14 @@ char *MSG_ReadStringLine( msg_t *msg ) {
 		if ( c > 127 ) {
 			c = '.';
 		}
-
-		string[l] = c;
-		l++;
-	} while (l < sizeof(string)-1);
+		// break only after reading all expected data from bitstream
+		if ( l >= sizeof(string)-1 ) {
+			break;
+		}
+		string[l++] = c;
+	} while (1);
 	
-	string[l] = 0;
+	string[l] = '\0';
 	
 	return string;
 }
