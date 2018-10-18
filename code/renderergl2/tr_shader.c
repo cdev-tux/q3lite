1918f14e0d696ba8429419c83ae1cdbf35f80e68
cdev-tux <cdev-tux@users.noreply.github.com>
2018-10-18 00:41:23 -0600

    OpenGL2: Fix parsing specularScale in shaders.
    Fix specularScale <metallic> <smoothness> with r_pbr 1 which has been
    broken since r_pbr was implemented in 2016.
    
    Fix specularScale <r> <g> <b> <gloss> setting b to gloss and leaving
    gloss as 0 since it was implemented in 2014.
    
    ioquake/ioq3@42d2fb9

--- code/renderergl2/tr_shader.c
+++ code/renderergl2/tr_shader.c
@@ -1122,6 +1122,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
 					stage->specularScale[1] = (stage->specularScale[0] < 0.5f) ? 0.0f : 1.0f;
 					stage->specularScale[0] = smoothness;
 				}
+				else
 				{
 					// two values, rgb then gloss
 					stage->specularScale[3] = stage->specularScale[1];
@@ -1140,7 +1141,7 @@ static qboolean ParseStage( shaderStage_t *stage, char **text )
 				continue;
 			}
 
-			stage->specularScale[2] = atof( token );
+			stage->specularScale[3] = atof( token );
 
 		}
 		//
