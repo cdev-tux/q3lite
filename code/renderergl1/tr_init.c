d73a0b329443477237bd46c03674e393658f446b
cdev-tux <cdev-tux@users.noreply.github.com>
2018-10-19 19:47:08 -0600

    Fix renderer not clearing some GL extension variables at vid_restart.
    If renderer is compiled into client (USE_RENDERER_DLOPEN=0) and
    after start up set r_allowExtension to 0 and run vid_restart, some
    extensions were still used.
    
    ioquake/ioq3@bead4aa

--- code/renderergl1/tr_init.c
+++ code/renderergl1/tr_init.c
@@ -1292,6 +1292,10 @@ void RE_Shutdown( qboolean destroyWindow ) {
 		GLimp_Shutdown();
 
 		Com_Memset( &glConfig, 0, sizeof( glConfig ) );
+		textureFilterAnisotropic = qfalse;
+		maxAnisotropy = 0;
+		displayAspect = 0.0f;
+
 		Com_Memset( &glState, 0, sizeof( glState ) );
 	}
 
